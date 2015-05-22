c096bad8eab7651cefadaa99145527f4da1540e1
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-22 10:06:18 -0400

    change how `option` is defined in tests

--- test/test.js
+++ test/test.js
@@ -8,38 +8,39 @@
 'use strict';
 
 var assert = require('assert');
-var option = require('../');
+var legacyOption = require('../');
+var option;
 
 describe('option', function () {
   before(function(done) {
-    this.option = option.create();
+    option = legacyOption.create();
     done();
   });
 
   it('option.init', function() {
     var expected = {foo:'bar', bool:true, 'bar':{foo:'bar'}};
-    assert.deepEqual(this.option.init(expected), expected);
+    assert.deepEqual(option.init(expected), expected);
   });
 
   it('option', function() {
-    assert.equal(this.option('foo', 'bar'), this.option('foo'));
+    assert.equal(option('foo', 'bar'), option('foo'));
 
-    this.option('foo', {foo:'bar'});
-    assert.deepEqual(this.option('foo'), {foo:'bar'});
-    assert.equal(this.option('no-there'), false);
+    option('foo', {foo:'bar'});
+    assert.deepEqual(option('foo'), {foo:'bar'});
+    assert.equal(option('no-there'), false);
 
-    this.option('there', false);
-    assert.equal(this.option('no-there'), true);
+    option('there', false);
+    assert.equal(option('no-there'), true);
   });
 
   it('option.flags', function() {
-    this.option.init({
+    option.init({
       foo: 'bar',
       there: true,
       obj: {foo: 'bar'},
       arr: []
     });
-    assert.deepEqual(this.option.flags(), ['--foo=bar', '--there', '--obj=[object Object]']);
+    assert.deepEqual(option.flags(), ['--foo=bar', '--there', '--obj=[object Object]']);
   });
 });
 
